544f1cbbc2b670573b28c881f49cc91bf19f69c6
codementee <codementee@outlook.com>
2021-05-12 22:26:09 +0500

    update

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const openIcon = document.querySelectorAll('.toggle__btn')[0];
 const closeIcon = document.querySelectorAll('.toggle__btn')[1];
 const navContainer = document.querySelector('.page-header__nav-whts-container');
